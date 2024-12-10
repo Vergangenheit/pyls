fda8d3844473e5a3b357a94088c26be3dca1ff4e
Loloheia <lorenzo.ostano@vx.se>
2024-12-10 11:04:53 +0100

    subtask2 supported

--- parser/parser.py
+++ parser/parser.py
@@ -1,31 +1,62 @@
-from typing import List
+from typing import List, Dict, Union
 import json
 from argparse import Namespace
 import os
 
-def parser_function(args: Namespace) -> List:
-    if args is None:
-        raise ValueError("args is required")
-    if args.file == "":
+def parser_function(filepath: str, options: List[str]) -> List:
+    data = extract_from_file(filepath)
+    # collect all the options
+    options_map = {"A": option_a, "ls": option_ls}
+
+    # output the data in "contents" key to the console following linux ls protocol
+    for option in options:
+        if option in options_map:
+            data = options_map[option](data)
+
+    return data
+
+def option_ls(data: Union[Dict, List]) -> List:
+    if isinstance(data, list):
+        contents = []
+        for content in data:
+            if content.startswith("."):
+                continue
+            contents.append(content)
+
+        return contents
+    elif isinstance(data, Dict):
+        contents = []
+        for content in data["contents"]:
+            if content["name"].startswith("."):
+                continue
+            contents.append(content["name"])
+
+        return contents
+    
+
+def option_a(data: Dict) -> List:
+    contents = []
+    for content in data["contents"]:
+        if isinstance(content["name"], str):
+            contents.append(content["name"])
+
+    return contents
+
+
+def extract_from_file(filepath: str) -> Dict:
+    if filepath == "":
         raise ValueError("file path is required")
-    if os.path.exists(args.file) is False:
+    if os.path.exists(filepath) is False:
         raise ValueError("file does not exist")
     # open the file and if it is not a json file, raise an error
-    with open(args.file) as file:
+    with open(filepath) as json_file:
         try:
-            data = json.load(file)
+            data = json.load(json_file)
         except json.JSONDecodeError:
             raise ValueError("file is not a json file")
     # check if the deserialized data is not empty
     if not data:
         raise ValueError("json data is empty")
-    # output the data in "contents" key to the console following linux ls protocol
-    contents = []
-    for content in data["contents"]:
-        if isinstance(content["name"], str):
-            if content["name"].startswith("."):
-                continue
-            contents.append(content["name"])
-
-    return contents
+    
+    return data
             
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,35 +7,41 @@ import json
 
 def test_nofile():
     with raises(ValueError, match="file path is required"):
-        parser_function(Namespace(file=""))
-
-def test_noargs():
-    with raises(ValueError, match="args is required"):
-        parser_function(None)
+        parser_function("", [])
 
 def test_file_not_exist():
     with raises(ValueError, match="file does not exist"):
-        parser_function(Namespace(file="nonexistent.json"))
+        parser_function("nonexistent.json", [])
 
 def test_file_not_json():
     with raises(ValueError, match="file is not a json file"):
-        parser_function(Namespace(file="tests/test_parser.py"))
+        parser_function("tests/test_parser.py", [])
 
 def test_empty_json():
     empty_dict = {}
     with open("tests/empty.json", "w") as file:
         json.dump(empty_dict, file, indent=4)
     with raises(ValueError, match="json data is empty"):
-        parser_function(Namespace(file="tests/empty.json"))
+        parser_function("tests/empty.json", [])
     os.remove("tests/empty.json")
 
-def test_hidden_files():
+def test_ls_only():
     test_data = {"contents": [{"name": ".hidden"}, {"name": "visible"}]}
     with open("tests/hidden.json", "w") as file:
         json.dump(test_data, file, indent=4)
-    data = parser_function(Namespace(file="tests/hidden.json"))
+    data = parser_function("tests/hidden.json", ["ls"])
 
     assert data == ["visible"]
+
+    os.remove("tests/hidden.json")
+
+def test_a_only():
+    test_data = {"contents": [{"name": ".hidden"}, {"name": "visible"}]}
+    with open("tests/hidden.json", "w") as file:
+        json.dump(test_data, file, indent=4)
+    data = parser_function("tests/hidden.json", ["A"])
+
+    assert data == [".hidden", "visible"]
 
     os.remove("tests/hidden.json")
     
