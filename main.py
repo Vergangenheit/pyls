84aa4f53a823b7665456f35505714d78df214467
Loloheia <lorenzo.ostano@vx.se>
2024-12-11 14:22:34 +0100

    remove file used just for testing

--- main.py
+++
@@ -1,8 +0,0 @@
-# TODO
-# REMOVE THIS FILE BEFORE SUBMISSION
-
-
-if __name__ == "__main__":
-    from parser.parser import parser_function
-    data = parser_function("structure.json", ["l", "t", "r"])
-    print(f'Parsed data: {data}')
